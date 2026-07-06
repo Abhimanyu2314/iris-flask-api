886fd7b6122562ba19d29ad3a93044f7dcafd258
Abhimanyu2314 <abhiabha6@gmail.com>
2026-07-06 10:35:13 +0530

    Add web UI frontend template

--- test_api.py
+++ test_api.py
@@ -1,11 +1,15 @@
 import requests
 
-url = 'http://127.0.0.1:5000/predict' # Switched to explicit local IP
+# Your unique live URL from the Render dashboard
+url = 'https://iris-flask-api-eax0.onrender.com/predict'
+
 data = {'features': [5.1, 3.5, 1.4, 0.2]}
 
 try:
+    print("Sending data to your live cloud server...")
     response = requests.post(url, json=data)
+    
     print("Status Code:", response.status_code)
-    print("Raw Response Text:", response.text)
+    print("Live API Response:", response.json())
 except Exception as e:
-    print("Connection Error:", e)
+    print("An error occurred connecting to the cloud:", e)
